8d6b0ba2903a96c2c792187273d4551e31105cce
arjoo <2009arjoo@gmail.com>
2024-02-08 22:01:54 +0530

    initial commit

---
+++ Sample-App/src/test/java/com/learning/Sample/App/SampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.Sample.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
